c6e2eed29bb11bf15a3dffcf4833bf74ab6a610b
Adriano RPL <adriano.pogianeli@gmail.com>
2018-08-07 23:23:19 -0300

    Add random action on ball in startup
    Add random action on ball in startup

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -1,53 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BallControl : MonoBehaviour
-{
-
-    private Rigidbody2D rb2d;
-
-    void GoBall()
-    {
-        float rand = Random.Range(0, 2);
-        if (rand < 1)
-        {
-            rb2d.AddForce(new Vector2(20, -15));
-        }
-        else
-        {
-            rb2d.AddForce(new Vector2(-20, -15));
-        }
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
-        Invoke("GoBall", 2);
-    }
-
-    void ResetBall()
-    {
-        rb2d.velocity = new Vector2(0, 0);
-        transform.position = Vector2.zero;
-    }
-
-    void RestartGame()
-    {
-        ResetBall();
-        Invoke("GoBall", 1);
-    }
-
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.collider.CompareTag("Player"))
-        {
-            Vector2 vel;
-            vel.x = rb2d.velocity.x;
-            vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
-            rb2d.velocity = vel;
-        }
-    }
-
-}
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class BallControl : MonoBehaviour {      private Rigidbody2D rb2d;      void GoBall()     {         float rand = Random.Range(0, 2);         if (rand < 1)         {             rb2d.AddForce(new Vector2(20, -15));         }         else         {             rb2d.AddForce(new Vector2(-20, -15));         }          int xDirection = Random.Range(0, 2);         int yDirection = Random.Range(0, 3);          Vector3 launchDirectorion = new Vector3();          if (xDirection == 0)         {             launchDirectorion.x = -8f;         }         if (xDirection == 1)         {             launchDirectorion.x = 8f;         }          if (yDirection == 0)         {             launchDirectorion.y = -8f;         }         if (yDirection == 1)         {             launchDirectorion.y = 8f;         }         if (yDirection == 2)         {             launchDirectorion.y = 0f;         }          rb2d.velocity = launchDirectorion;      }      // Use this for initialization     void Start()     {         rb2d = GetComponent<Rigidbody2D>();         Invoke("GoBall", 2);     }      void ResetBall()     {         rb2d.velocity = new Vector2(0, 0);         transform.position = Vector2.zero;     }      void RestartGame()     {         ResetBall();         Invoke("GoBall", 1);     }      void OnCollisionEnter2D(Collision2D coll)     {         if (coll.collider.CompareTag("Player"))         {             Vector2 vel;             vel.x = rb2d.velocity.x;             vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);             rb2d.velocity = vel;         }     }  } 
